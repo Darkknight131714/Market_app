d0c6d12ce7301f3bc00e4b95af2fc8b9f33c5be2
Darkknight131714 <darkknight131714@gmail.com>
2021-09-21 01:05:58 +0530

    Added UPI

--- lib/buy.dart
+++ lib/buy.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'item.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:upi_india/upi_india.dart';
+import 'package:upi_pay/upi_pay.dart';
 final _firestore= FirebaseFirestore.instance;
+UpiIndia _upiIndia = UpiIndia();
+List<ApplicationMeta> appMetaList=[];
+
 class BuyScreen extends StatefulWidget {
   @override
   int cost;
@@ -20,22 +25,29 @@ void sendOrder(List<Item> cart)async{
     order+=cart[i].quantity.toString();
     order+="\n";
   }
-  // final Email email = Email(
-  //   body:order,
-  //   subject: "New Order",
-  //   recipients: ["darkknight131714@gmail.com"],
-  //   isHTML: true,
-  // );
-  // await FlutterEmailSender.send(email);
-  print("Mail Sent");
 }
+
 class _BuyScreenState extends State<BuyScreen> {
+  UpiApp app = UpiApp.googlePay;
+  List<ApplicationMeta> _apps=[];
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(milliseconds: 0),()async{
+      _apps = await UpiPay.getInstalledUpiApplications(statusType: UpiApplicationDiscoveryAppStatusType.all);
+      setState(() {});
+      print("KK");
+      for(int i=0;i<_apps.length;i++){
+        print(_apps[i].upiApplication);
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     int cost=widget.cost;
     List<Item> cart=widget.cart;
     print(cost.toString());
     Firebase.initializeApp();
+    getApps();
     return Material(
       type: MaterialType.transparency,
       child: SafeArea(
@@ -54,10 +66,39 @@ class _BuyScreenState extends State<BuyScreen> {
                     'address': 'A wing 1104',
                   }
               );
+              doUpiTransaction(appMetaList[0]);
             }, child: Text("Buy")),
           ],
         ),
       ),
     );
   }
+}
+Future<UpiResponse> initiateTransaction(UpiApp app) async {
+  return _upiIndia.startTransaction(
+    app: app,
+    receiverUpiId: "Divyanshumeena321@okicici",
+    receiverName: 'Divyanshu',
+    transactionRefId: 'Testing',
+    transactionNote: 'Not actual. Just an example.',
+    amount: 1.00,
+  );
+}
+Future<void> getApps() async{
+  appMetaList = await UpiPay.getInstalledUpiApplications(statusType: UpiApplicationDiscoveryAppStatusType.all);
+  print("Bye");
+  for(int i=0;i<appMetaList.length;i++){
+    print(appMetaList[i].upiApplication);
+  }
+}
+Future doUpiTransaction(ApplicationMeta appMeta) async {
+  final UpiTransactionResponse response = await UpiPay.initiateTransaction(
+    amount: '10.00',
+    app: appMeta.upiApplication,
+    receiverName: 'Divyanshu',
+    receiverUpiAddress: 'Divyanshumeena321@okicici',
+    transactionRef: 'UPITXREF0001',
+    transactionNote: 'A UPI Transaction',
+  );
+  print(response.status);
 }
